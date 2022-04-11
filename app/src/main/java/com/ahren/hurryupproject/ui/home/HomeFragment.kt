c7ffda2a015f0fb8c08f1bd4dd0e023f662f9fe3
JiahangChen <chjh171009@gmail.com>
2022-04-12 03:06:11 +0800

    create move state feature

--- app/src/main/java/com/ahren/hurryupproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ahren/hurryupproject/ui/home/HomeFragment.kt
@@ -1,25 +1,28 @@
 package com.ahren.hurryupproject.ui.home
 
 import android.app.Activity.RESULT_OK
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.Dimension.DP
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
-import com.ahren.hurryupproject.R
 import com.ahren.hurryupproject.databinding.FragmentHomeBinding
 import com.ahren.hurryupproject.ui.addstation.AddStationActivity
 
+
 class HomeFragment : Fragment() {
 
-    private lateinit var register : ActivityResultLauncher<Intent>
+    private lateinit var register: ActivityResultLauncher<Intent>
 
     private val homeViewModel: HomeViewModel by activityViewModels()
     private var _homeViewBinding: FragmentHomeBinding? = null
@@ -28,7 +31,7 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val homeViewBinding get() = _homeViewBinding!!
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
@@ -39,19 +42,32 @@ class HomeFragment : Fragment() {
     ): View {
         _homeViewBinding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        register = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK){
-                homeViewModel.addStation(requireContext(), result.data?.getStringExtra("lineid")!!, result.data?.getStringExtra("stationid")!!)
-                refreshStationList()
+        register =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    homeViewModel.addStation(
+                        requireContext(),
+                        result.data?.getStringExtra("lineid")!!,
+                        result.data?.getStringExtra("stationid")!!
+                    )
+                    refreshStationList()
 
+                }
             }
-        }
         refreshStationList()
 
         homeViewBinding.floatingCreateStationButton.setOnClickListener {
 
-            register.launch(Intent(requireContext(),AddStationActivity::class.java))
+            register.launch(Intent(requireContext(), AddStationActivity::class.java))
 
+        }
+
+        homeViewBinding.stationup.setOnClickListener {
+            moveStatePosition(false)
+        }
+
+        homeViewBinding.stationdown.setOnClickListener {
+            moveStatePosition(true)
         }
 
         return homeViewBinding.root
@@ -70,16 +86,41 @@ class HomeFragment : Fragment() {
 
     fun refreshStationList() {
         homeViewModel.getStationList().observe(viewLifecycleOwner) {
-            homeViewBinding.textView.text = it[0]._stationName.get()
-            homeViewBinding.textView2.text = it[1]._stationName.get()
-            homeViewBinding.textView3.text = it[2]._stationName.get()
-            homeViewBinding.textView4.text = it[3]._stationName.get()
-            homeViewBinding.textView5.text = it[4]._stationName.get()
-            homeViewBinding.textView.background = it[0]._backgroundColor.get()
-            homeViewBinding.textView2.background = it[1]._backgroundColor.get()
-            homeViewBinding.textView3.background = it[2]._backgroundColor.get()
-            homeViewBinding.textView4.background = it[3]._backgroundColor.get()
-            homeViewBinding.textView5.background = it[4]._backgroundColor.get()
+            homeViewBinding.StationNum1.text = it[0]._stationName.get()
+            homeViewBinding.StationNum2.text = it[1]._stationName.get()
+            homeViewBinding.StationNum3.text = it[2]._stationName.get()
+            homeViewBinding.StationNum4.text = it[3]._stationName.get()
+            homeViewBinding.StationNum5.text = it[4]._stationName.get()
+            homeViewBinding.StationNum1.background = it[0]._backgroundColor.get()
+            homeViewBinding.StationNum2.background = it[1]._backgroundColor.get()
+            homeViewBinding.StationNum3.background = it[2]._backgroundColor.get()
+            homeViewBinding.StationNum4.background = it[3]._backgroundColor.get()
+            homeViewBinding.StationNum5.background = it[4]._backgroundColor.get()
         }
     }
+
+    fun moveStatePosition(direction: Boolean) {
+        val StateMovingValue by lazy { homeViewBinding.StationNum2.height + homeViewBinding.StationNum2.marginTop }
+
+        val marginTopChangedVale by lazy {
+            if (direction) {
+                homeViewModel.getDownState(StateMovingValue)
+            } else {
+                homeViewModel.getUpState(StateMovingValue)
+            }
+        }
+        val c = ConstraintSet()
+        c.clone(homeViewBinding.homeconstraintlayout)
+        c.connect(
+            homeViewBinding.youarehere.id,
+            ConstraintSet.TOP,
+            ConstraintSet.PARENT_ID,
+            ConstraintSet.TOP,
+            homeViewBinding.youarehere.marginTop + marginTopChangedVale
+        )
+        c.applyTo(homeViewBinding.homeconstraintlayout)
+
+
+    }
+
 }
--- app/src/main/java/com/ahren/hurryupproject/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ahren/hurryupproject/ui/home/HomeViewModel.kt
@@ -6,24 +6,26 @@ import android.content.ContentValues.TAG
 import android.content.Context
 import android.graphics.drawable.ColorDrawable
 import android.util.Log
+import android.util.TypedValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ahren.hurryupproject.MyApplication
 import com.ahren.hurryupproject.R
+import com.ahren.hurryupproject.databinding.FragmentHomeBinding
 import com.ahren.hurryupproject.ui.addstation.bean.StationListBindingData
 import com.ahren.hurryupproject.ui.addstation.bean.StationListDataClass
 import com.ahren.hurryupproject.ui.addstation.datareader.StationDataReader
 
-class HomeViewModel : ViewModel( ){
+class HomeViewModel(application : Application) : AndroidViewModel(application) {
 
     @SuppressLint("ResourceAsColor")
     private var emptyStationData = StationListBindingData("","","",0.00,0.00, ColorDrawable(0x3ccccccc), true)
     val _stationlist by lazy {
         MutableLiveData(arrayListOf(emptyStationData, emptyStationData, emptyStationData, emptyStationData, emptyStationData))
     }
-
+    private var stateButtonPosition: Int = 0
     fun addStation(context: Context, lineid:String, stationid: String) {
         for ( i in 0 until _stationlist.value!!.size) {
             if (_stationlist.value!![i].isEmpty){
@@ -46,4 +48,29 @@ class HomeViewModel : ViewModel( ){
         return _stationlist
     }
 
+    fun getUpState(dpValue: Int): Int {
+        if (stateButtonPosition > 0) {
+            if (!_stationlist.value!![stateButtonPosition - 1].isEmpty) {
+                stateButtonPosition--
+                return - dpValue
+            }
+        }
+        return 0
+    }
+
+    fun getDownState(dpValue: Int): Int {
+        if (stateButtonPosition < 5) {
+            if (!_stationlist.value!![stateButtonPosition].isEmpty) {
+                stateButtonPosition++
+                return dpValue
+            }
+        }
+        return 0
+
+    }
+
+
+    fun convDpToInt(dpValue: Int): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), getApplication<Application>().resources.displayMetrics).toInt()
+    }
 }
